28054639908b194f26cec73405369a6abfa7fb0f
SIAM HOSSAIN <huffmancoding.acm@gmail.com>
2026-02-06 12:08:40 +0600

    Solve Array Problem DSA

---
+++ Greeks For Greeks/Array/Rotate An array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Function to rotate an array by d elements to the left
+void rotateArr(vector<int>& arr, int d) {
+    int n = arr.size();
+  
+    // Handle the case where d > size of array
+    d %= n;
+  
+    // Reverse the first d elements
+    reverse(arr.begin(), arr.begin() + d);
+
+    // Reverse the remaining n-d elements
+    reverse(arr.begin() + d, arr.end());
+  
+    // Reverse the entire array
+    reverse(arr.begin(), arr.end());
+}
